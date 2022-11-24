64e7e7c8bb8ce5e96815d77fe707580cd5623531
Jonathan Parra <jhonp0991@gmail.com>
2022-11-24 17:31:00 -0500

    puerto express funcionando

--- Node Example/app.js
+++ Node Example/app.js
@@ -1,1 +1,10 @@
-console.log("hola mundo! ");
+import express from "express";
+
+const app = express();
+const puerto = 3000;
+
+function callback(){
+    console.log('El servidor se esta ejecutando');
+}
+
+app.listen(puerto, callback)
