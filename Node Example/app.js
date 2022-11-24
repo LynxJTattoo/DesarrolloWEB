5a901d60b3ef9cf0243e4f44d624bc225c16e780
Jonathan Parra <jhonp0991@gmail.com>
2022-11-24 18:03:16 -0500

    express home

--- Node Example/app.js
+++ Node Example/app.js
@@ -3,8 +3,22 @@ import express from "express";
 const app = express();
 const puerto = 3000;
 
-function callback(){
-    console.log('El servidor se esta ejecutando');
-}
 
-app.listen(puerto, callback)
+app.listen(puerto, () => {
+    console.log('El servidor se esta ejecutando');
+})
+
+app.get('/',(req, res) =>{
+    //Implementar el codigo
+    res.send('Hola Mundo')
+})
+
+app.get('/home',(req, res) =>{
+    //Implementar el codigo
+    res.send('Estas en home')
+})
+
+app.get('/home/group',(req, res) =>{
+    //Implementar el codigo
+    res.send('Estas en group')
+})
